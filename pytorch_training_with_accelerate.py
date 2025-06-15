cd555c0a5793c8882176264c628bfe6bd7769d8c
Steven Accardo <accardo.steven@gmail.com>
2025-06-14 17:56:37 -0700

    Small update.

--- pytorch_training_with_accelerate.py
+++ pytorch_training_with_accelerate.py
@@ -126,17 +126,3 @@ for epoch in range(num_epochs):
     optimizer.zero_grad()
     progress_bar.update(1)
     
-import evaluate
-
-metric = evaluate.load("glue", "mrpc")
-model.eval()
-for batch in eval_dataloader:
-  batch = {k: v.to(device) for k, v in batch.items()}
-  with torch.no_grad():
-    outputs = model(**batch)
-
-  logits = outputs.logits
-  predictions = torch.argmax(logits, dim=-1)
-  metric.add_batch(predictions=predictions, references=batch["labels"])
-
-metric.compute()
